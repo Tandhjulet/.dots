2fe4d92b48a4328413aac64f5aac214c02422044
Mads <madsbechmortensen@hotmail.dk>
2026-07-21 22:28:21 +0200

    feat: begin top waybar config

--- modules/desktop/common/waybar/top/config.nix
+++ modules/desktop/common/waybar/top/config.nix
@@ -1,3 +1,40 @@
 {
+  mainBar = {
+    layer = "top";
+    position = "top";
+    margin-bottom = 0;
+    margin-left = 0;
+    margin-right = 0;
 
+    spacing = 0;
+    tooltip = true;
+    reload_style_on_change = true;
+
+    modules-left = [ "mpris" ];
+    modules-center = [
+      "clock"
+      "custom/menu"
+      "niri/workspaces"
+      # hyprland/workspaces
+      # mango/workspaces
+      "custom/recorder"
+      "group/monitor"
+      "group/bar"
+    ];
+    modules-right = [
+      "backlight"
+      "backlight/slider"
+      "pulseaudio"
+      "pulseaudio/slider"
+      "custom/notification"
+    ];
+
+    "niri/workspaces" = {
+      format = "{icon}";
+      format-icons = {
+        active = "";
+        default = "";
+      };
+    };
+  };
 }

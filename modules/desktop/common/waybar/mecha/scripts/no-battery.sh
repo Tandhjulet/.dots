0f8db72eabf2ee25cf5e0c8e181bd3cacf595cf6
Mads <madsbechmortensen@hotmail.dk>
2026-07-22 12:46:45 +0200

    feat: no battery fallback!

--- modules/desktop/common/waybar/mecha/scripts/no-battery.sh
+++ modules/desktop/common/waybar/mecha/scripts/no-battery.sh
@@ -3,5 +3,7 @@
 BAT=$(find /sys/class/power_supply/ -maxdepth 1 -name "BAT*" 2>/dev/null | head -n1)
 
 if [ -z "$BAT" ]; then
-    echo "󰚥"
+    echo '{ "alt": "no-battery", "class": "no-battery" }'
+else
+	echo '{ "alt": "battery", "class": "battery" }'
 fi

1e12ce36cf0d27a5932f406d218de68797fe90ff
Mads <madsbechmortensen@hotmail.dk>
2026-07-25 12:30:34 +0200

    feat: finally made matugen work imperatively

--- home/theming/default.nix
+++ home/theming/default.nix
@@ -19,21 +19,30 @@
     enable = true;
 
     settings = {
+      config = {
+        version_check = false;
+
+        # wallpaper = {
+        #   set = true;
+        #   command = "awww img --transition-type center {{ image }}";
+        # };
+      };
+
       templates = {
         waybar = {
           input_path = ./templates/colors.css;
-          output_path = "$HOME/waybar/colors.css";
-          post_hook = "pkill -SIGUSR2 waybar";
+          output_path = "${config.xdg.configHome}/waybar/colors.css";
+          post_hook = "pkill -SIGUSR2 waybar || :";
         };
 
         rofi = {
           input_path = ./templates/rofi-colors.rasi;
-          output_path = "$HOME/rofi/colors.rasi";
+          output_path = "${config.xdg.configHome}/rofi/colors.rasi";
         };
 
         swaync = {
           input_path = ./templates/colors.css;
-          output_path = "$HOME/swaync/colors.css";
+          output_path = "${config.xdg.configHome}/swaync/colors.css";
           post_hook = "swaync-client -rs";
         };
       };

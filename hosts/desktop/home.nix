f33f391156a796d0eccb2533716d1a20bde94f62
Mads <madsbechmortensen@hotmail.dk>
2026-08-23 19:03:27 +0200

    Override spicetify theme to text on desktop host

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -11,6 +11,8 @@
     { name = "DP-3"; width = 1920; height = 1080; refresh = 60.0; x = -1920; y = 0; scale = 1.0; }
   ];
 
+  my.programs.spicetify.theme = "text";
+
   programs.git.settings.user = {
     name = "Mads";
     email = "madsbechmortensen@hotmail.dk";
